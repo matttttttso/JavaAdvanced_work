fd0b8063ca8aaa09f93a7145ed8fcb1d16819c83
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-05-01 23:26:40 +0900

    Describe function of Searching with Pref name & City name and correct totaly
    都道府県名・市町村名検索の機能実装
    あと全体的な修正

--- p12-7_DB_access/src/Main_12_7.java
+++ p12-7_DB_access/src/Main_12_7.java
@@ -8,53 +8,49 @@ import java.util.Scanner;
 
 public class Main_12_7 {
 	public static void main(String[] args) {
-		final String QUIT = "q";
-		final int ZERO = 0;
-		String input;
-		String pnq;
-		String searchZipcode;
-		Scanner scanner = new Scanner(System.in);
-		int indexRow = ZERO;
-
+		final	String	QUIT = "q";
+		final	int		ZERO = 0;
+		int		indexRow = ZERO,
+				numberOfRows = ZERO;
+		String	input,
+				pnq = null,
+				searchZipcode,
+				searchPref,
+				searchCity;
+		Scanner scanner = new Scanner(System.in, "UTF-8");
 
 		try {
 			Class.forName("org.h2.Driver");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-
 		try (
 			Connection connection = DriverManager.getConnection("jdbc:h2:~\\exercisedb", "sa", "");
 			) {
 			connection.setAutoCommit(false);
-
 			try (
-					Statement stmt = connection.createStatement()) {
+					Statement stmt = connection.createStatement()
+				) {
 				String sql = "CREATE TABLE"
 						+ " IF NOT EXISTS ZIP_CODE"
 						+ " AS SELECT *"
 						+ " FROM CSVREAD('~\\Documents\\zipcode\\zip_data_split_1.csv', null, 'UTF-8', ',')";
 				stmt.executeUpdate(sql);
-
 				sql = "INSERT INTO ZIP_CODE SELECT *"
 						+ " FROM CSVREAD('~\\Documents\\zipcode\\zip_data_split_2.csv', null, 'UTF-8', ',')"
 						+ " AS TMP WHERE NOT EXISTS(SELECT * FROM ZIP_CODE WHERE ZIP_CODE = '9570036')";
 				stmt.executeUpdate(sql);
-
 				sql = "INSERT INTO ZIP_CODE SELECT *"
 						+ " FROM CSVREAD('~\\Documents\\zipcode\\zip_data_split_3.csv', null, 'UTF-8', ',')"
 						+ " AS TMP WHERE NOT EXISTS(SELECT * FROM ZIP_CODE WHERE ZIP_CODE = '6750023')";
 				stmt.executeUpdate(sql);
-
 				connection.commit();
 			} catch (SQLException e) {
 				connection.rollback();
 				throw e;
 			}
 			System.out.println("テーブルの作成、レコードの追加が完了しました。");
-
 			connection.setAutoCommit(true);
-
 			LOOP:while (true) {
 				printMenu();
 				INPUT:while(true) {
@@ -89,14 +85,20 @@ public class Main_12_7 {
 									System.out.printf(" %7s \t %5s \t %10s \t %15s \n", zipcode, pref, city, town);
 								}
 								INPUT_LOOP: while(true) {
-									if(indexRow == ZERO) {
-										System.out.print("[次の10件を表示:n] [終了:q] >");
-									}else if(indexRow >= 10){
-										System.out.print("[前の10件を表示:p] [次の10件を表示:n] [終了:q] >");
-									}
 									try {
-										pnq = scanner.next().toLowerCase();
-										checkPNQ(pnq);
+										if(indexRow == ZERO) {
+											System.out.print("[次の10件を表示:n] [終了:q] >");
+											pnq = scanner.next().toLowerCase();
+											checkNQ(pnq);
+										} else if(numberOfRows == 10 && indexRow >= 10){
+											System.out.print("[前の10件を表示:p] [次の10件を表示:n] [終了:q] >");
+											pnq = scanner.next().toLowerCase();
+											checkPNQ(pnq);
+										} else if(numberOfRows < 10 && indexRow != ZERO) {
+											System.out.print("[前の10件を表示:p] [終了:q] >");
+											pnq = scanner.next().toLowerCase();
+											checkPQ(pnq);
+										}
 										break INPUT_LOOP;
 									} catch (InputOutOfBoundException e) {
 										System.out.println(e.getMessage());
@@ -174,6 +176,88 @@ public class Main_12_7 {
 					break SWITCH;
 				case "t":
 					System.out.println("\n都道府県名・市町村名検索----------------------------");
+					System.out.print("検索する都道府県名を入力してください。 [終了:q] >");
+					searchPref = scanner.next();
+					System.out.println("入力値：" + searchPref);
+					System.out.print("検索する市町村名を入力してください。 [終了:q] >");
+					searchCity = scanner.next();
+					System.out.println("入力値：" + searchCity);
+					indexRow = 0;
+					T_LOOP: while(true) {
+						try(
+							PreparedStatement pstmt = connection.prepareStatement("SELECT ZIP_CODE, PREF, CITY, TOWN FROM ZIP_CODE"
+									+ " WHERE PREF = ? AND CITY = ? LIMIT ?,10");
+							) {
+							if(searchPref.equals(QUIT) || searchCity.equals(QUIT)) {
+								System.out.println("条件検索を終了し、メニューに戻ります。\n");
+								break T_LOOP;
+							} else {
+								pstmt.setString(1, searchPref);
+								pstmt.setString(2, searchCity);
+								pstmt.setInt(3, indexRow);
+							}
+							try (
+								ResultSet rs = pstmt.executeQuery();
+								) {
+								System.out.println("郵便番号\t\t都道府県名\t\t市\t\t\t\t町名");
+								while (rs.next()) {
+									String zipcode = rs.getString(1);
+									String pref = rs.getString(2);
+									String city = rs.getString(3);
+									String town = rs.getString(4);
+									System.out.printf(" %7s \t %5s \t %10s \t %15s \n", zipcode, pref, city, town);
+									numberOfRows = rs.getRow();
+								}
+							}
+							if(numberOfRows == 0 && indexRow == ZERO) {
+								System.out.println("見つかりませんでした。");
+								System.out.println("都道府県名・市町村名検索を終了し、メニューに戻ります。\n");
+								break T_LOOP;
+							} else if(numberOfRows < 10 && indexRow == ZERO) {
+								System.out.println("検索結果：" + numberOfRows + "件");
+								System.out.println("都道府県名・市町村名検索を終了し、メニューに戻ります。\n");
+								break T_LOOP;
+							} else {
+								INPUT_LOOP: while(true) {
+									try {
+										if(indexRow == ZERO) {
+											System.out.print("[次の10件を表示:n] [終了:q] >");
+											pnq = scanner.next().toLowerCase();
+											checkNQ(pnq);
+										} else if(numberOfRows == 10 && indexRow >= 10){
+											System.out.print("[前の10件を表示:p] [次の10件を表示:n] [終了:q] >");
+											pnq = scanner.next().toLowerCase();
+											checkPNQ(pnq);
+										} else if(numberOfRows < 10 && indexRow != ZERO) {
+											System.out.print("[前の10件を表示:p] [終了:q] >");
+											pnq = scanner.next().toLowerCase();
+											checkPQ(pnq);
+										}
+										break INPUT_LOOP;
+									} catch (InputOutOfBoundException e) {
+										System.out.println(e.getMessage());
+									}
+								}
+								PNQ: switch (pnq) {
+								case "p":
+									if(indexRow >= 10) {
+										indexRow -= 10;
+									}else if(indexRow == 0){
+										System.out.println("最初のページです。");
+									}
+									break PNQ;
+								case "n":
+									indexRow += 10;
+									break PNQ;
+								case "q":
+									System.out.println("都道府県名・市町村名検索を終了し、メニューに戻ります。\n");
+									break T_LOOP;
+								}
+							}
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+					}
 					break SWITCH;
 				case QUIT:
 					System.out.println("\nEND");
@@ -208,6 +292,16 @@ public class Main_12_7 {
 
 	public static void checkPNQ(String input) throws InputOutOfBoundException {
 		if (!input.equals("p") && !input.equals("n") && !input.equals("q")) {
+			throw new InputOutOfBoundException("指定された文字で入力してください。");
+		}
+	}
+	public static void checkNQ(String input) throws InputOutOfBoundException {
+		if (!input.equals("n") && !input.equals("q")) {
+			throw new InputOutOfBoundException("指定された文字で入力してください。");
+		}
+	}
+	public static void checkPQ(String input) throws InputOutOfBoundException {
+		if (!input.equals("p") && !input.equals("q")) {
 			throw new InputOutOfBoundException("指定された文字で入力してください。");
 		}
 	}
