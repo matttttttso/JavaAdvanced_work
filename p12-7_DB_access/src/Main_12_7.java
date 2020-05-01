a47462853d604f06ba7d27be91f91387cceab12b
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-05-01 14:26:38 +0900

    Describe function of Searching ALL

--- p12-7_DB_access/src/Main_12_7.java
+++ p12-7_DB_access/src/Main_12_7.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -67,6 +69,56 @@ public class Main_12_7 {
 					break SWITCH;
 				case "a":
 					System.out.println("全件検索 ----------------------------------------");
+
+					try (
+						PreparedStatement pstmt = connection.prepareStatement("SELECT ZIP_CODE, PREF, CITY, TOWN FROM ZIP_CODE LIMIT ?, 10");
+						) {
+						A_LOOP: while(true) {
+							pstmt.setInt(1, indexRow);
+							try (
+								ResultSet rs = pstmt.executeQuery();
+								) {
+								connection.commit();
+								System.out.println("郵便番号\t\t都道府県名\t\t市\t\t\t\t町名");
+								while (rs.next()) {
+									int zipcode = rs.getInt(1);
+									String pref = rs.getString(2);
+									String city = rs.getString(3);
+									String town = rs.getString(4);
+									System.out.printf(" %7d \t %5s \t %10s \t %15s \n", zipcode, pref, city, town);
+								}
+								if(indexRow == ZERO) {
+									System.out.print("[次の10件を表示:n] [終了:q] >");
+								}else if(indexRow >= 10){
+									System.out.print("[前の10件を表示:p] [次の10件を表示:n] [終了:q] >");
+								}
+								pnq = scanner.next().toLowerCase();
+								checkPNQ(pnq);
+								PNQ: switch (pnq) {
+								case "p":
+									if(indexRow >= 10) {
+										indexRow -= 10;
+									}else if(indexRow == 0){
+										System.out.println("最初のページです。");
+									}
+									break PNQ;
+								case "n":
+									indexRow += 10;
+									break PNQ;
+								case "q":
+									System.out.print("全件検索を終了します。");
+									break A_LOOP;
+								}
+							} catch (SQLException e) {
+								connection.rollback();
+								throw e;
+							} catch (InputOutOfBoundException e) {
+								System.out.println(e.getMessage());
+							}
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
 					break SWITCH;
 				case "s":
 					System.out.println("郵便番号検索 -------------------------------------");
@@ -101,6 +153,12 @@ public class Main_12_7 {
 
 	public static void checkInput(String input) throws InputOutOfBoundException {
 		if (!input.equals("c") && !input.equals("a") && !input.equals("s") && !input.equals("t") && !input.equals("q")) {
+			throw new InputOutOfBoundException("指定された文字で入力してください。");
+		}
+	}
+
+	public static void checkPNQ(String input) throws InputOutOfBoundException {
+		if (!input.equals("p") && !input.equals("n") && !input.equals("q")) {
 			throw new InputOutOfBoundException("指定された文字で入力してください。");
 		}
 	}
