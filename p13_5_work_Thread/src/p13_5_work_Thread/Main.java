b479d3a2c9870a9221032222238d0f4d3a37d902
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-05-07 10:08:56 +0900

    p13-5

--- p13_5_work_Thread/src/p13_5_work_Thread/Main.java
+++ p13_5_work_Thread/src/p13_5_work_Thread/Main.java
@@ -3,14 +3,14 @@ package p13_5_work_Thread;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String args[]) {
-
-		System.out.println("何か文字を入力してください！");
-		Thread showThread = new ShowThread();
-		showThread.start();
 		Scanner scan = new Scanner(System.in);
+		Thread showThread = new ShowThread();
+
+		System.out.print("何か文字を入力してください！>");
+		showThread.start();
 		String str = scan.next();
-		System.out.println("入力文字は " + str + " です。");
+		System.out.print("入力文字は " + str + "です。 ");
+		scan.close();
 	}
 }
