3e618f8e99d8622f3ad3d2efda1218be18b66850
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-05-08 15:28:58 +0900

    Update Card.java

--- p14_work1/src/p14_work1/Card.java
+++ p14_work1/src/p14_work1/Card.java
@@ -1,80 +1,51 @@
 package p14_work1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Card {
-	//フィールドメンバ
-	private String	suit,	//マークは２つ「スペード」 「ダイヤ」
-					num;
-	private int	cardInt;	//数字1~26 (2*13)
-	public final String	ACE = "A",
-						JACK = "J",
-						QUEEN = "Q",
-						KING = "K";
+	private final Rank rank;
+	private final Suit suit;
 
-	public int getCardInt() {
-		return cardInt;
+	private Card(Rank rank, Suit suit) {
+		this.rank = rank;
+		this.suit = suit;
 	}
-	//コンストラクタ
-	Card(int cardInt){
-		//数字と数字の表記の決定
-		this.cardInt = cardInt;
-		double cardDouble = cardInt;
-		this.num = String.valueOf(Math.ceil((cardDouble / 2) + 1));
-		switch(this.num) {
-			case "14":
-				this.num = ACE;
-				break;
-			case "11":
-				this.num = JACK;
-				break;
-			case "12":
-				this.num = QUEEN;
-				break;
-			case "13":
-				this.num = KING;
-				break;
-			default:
-				this.num = String.valueOf(this.num);
-				break;
-		}
-		//suitの決定
-		int mod = cardInt % 2;
-		switch(mod) {
-			case 0:
-				this.suit = "♠";
-				break;
-			case 1:
-				this.suit = "♦";
-				break;
-		}
+
+	public Rank rank() {
+		return rank;
 	}
-	/*
-	2が一番弱く、そこから数字が高い方が強くなり、Aは最も強いカードです。マークは関係なし
 
-	トランプの表し方
-	 数	♦ ♠
-	2：	1,2
-	3:	3,4
-	 ...
-	Q:	21,22
-	K:	23,24
-	A:	25,26
+	public Suit suit() {
+		return suit;
+	}
 
-	2で割ったあまり
-	 	1,0
-
-	2で割って+1
- 	2:	1.5 ~ 2.0
- 	3:	2.5 ~ 3.0
- 	 ...
- 	Q:	11.5 ~ 12.0
-	K:	12.5 ~ 13.0
-	A:	13.5 ~ 14.0
-	*/
-
-	//メソッドメンバ
-	//カードの表示
 	public String toString() {
-		return this.suit + "/" + this.num;
+		return suit.getSuit() + "/" + rank.getLabel();
+	}
+
+	private static final List<Card> protoDeck = new ArrayList<Card>();
+
+	// Initialize prototype deck
+	static {
+		for (Suit suit : Suit.values())
+			for (Rank rank : Rank.values())
+				protoDeck.add(new Card(rank, suit));
+	}
+
+	public static ArrayList<Card> newDeck() {
+		return new ArrayList<Card>(protoDeck); // Return copy of prototype deck
+	}
+
+	public String getLabel() {
+		return rank.getLabel() ;
+	}
+
+	public int getStrength() {
+		return rank.getStrength();
+	}
+
+	public String getSuit() {
+		return suit.getSuit();
 	}
 }
-
