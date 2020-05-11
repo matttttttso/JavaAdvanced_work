fdce8b138a32ba87ac97f03db1f1d7ce002a7014
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-05-11 10:22:28 +0900

    Separate Deck function as a class from Card.java

--- p14_work1/src/p14_work1/Card.java
+++ p14_work1/src/p14_work1/Card.java
@@ -1,13 +1,10 @@
 package p14_work1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Card {
 	private final Rank rank; //数字
 	private final Suit suit; //マーク
 
-	//getter
+//getter
 	//表示名
 	public String getLabel() {
 		return rank.getLabel();
@@ -21,25 +18,10 @@ public class Card {
 		return suit.getSuit();
 	}
 
-	//コンストラクタ
-	private Card(Rank rank, Suit suit) {
+//コンストラクタ
+	Card(Rank rank, Suit suit) {
 		this.rank = rank;
 		this.suit = suit;
-	}
-
-	private static final List<Card> protoDeck = new ArrayList<Card>();
-	//デッキの初期化
-	static {
-		for (Suit suit : Suit.values()) {
-			for (Rank rank : Rank.values()) {
-				protoDeck.add(new Card(rank, suit));
-			}
-		}
-	}
-
-	//初期化したデッキを返す
-	public static ArrayList<Card> newDeck() {
-		return new ArrayList<Card>(protoDeck);
 	}
 
 	//カードの表示用
---
+++ p14_work1/src/p14_work1/Deck.java
@@ -0,0 +1,30 @@
+package p14_work1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+	//デッキの初期化
+	private static final List<Card> protoDeck = new ArrayList<Card>();
+	static {
+		for (Suit suit : Suit.values()) {
+			for (Rank rank : Rank.values()) {
+				protoDeck.add(new Card(rank, suit));
+			}
+		}
+	}
+
+	//初期化したデッキを返す
+	public static ArrayList<Card> newDeck() {
+		return new ArrayList<Card>(protoDeck);
+	}
+
+	//カードを配る
+	static ArrayList<Card> deal(List<Card> deck, int n) {
+		int deckSize = deck.size();
+		List<Card> handView = deck.subList(deckSize - n, deckSize);
+		ArrayList<Card> hand = new ArrayList<Card>(handView);
+		handView.clear();
+		return hand;
+	}
+}
